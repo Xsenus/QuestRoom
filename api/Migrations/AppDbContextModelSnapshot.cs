7abc309d7c65c8ba95a7e8803d7f7f44c8748d94
Ilya <ilel@list.ru>
2026-01-28 12:26:56 +0700

    Remove quest gift fields migration

--- api/Migrations/AppDbContextModelSnapshot.cs
+++ api/Migrations/AppDbContextModelSnapshot.cs
@@ -553,14 +553,6 @@ namespace QuestRoomApi.Migrations
                         .HasColumnType("integer")
                         .HasColumnName("extra_participants_max");
 
-                    b.Property<string>("GiftGameLabel")
-                        .HasColumnType("text")
-                        .HasColumnName("gift_game_label");
-
-                    b.Property<string>("GiftGameUrl")
-                        .HasColumnType("text")
-                        .HasColumnName("gift_game_url");
-
                     b.PrimitiveCollection<string[]>("Images")
                         .IsRequired()
                         .HasColumnType("text[]")
@@ -612,10 +604,6 @@ namespace QuestRoomApi.Migrations
                     b.Property<DateTime>("UpdatedAt")
                         .HasColumnType("timestamp with time zone")
                         .HasColumnName("updated_at");
-
-                    b.Property<string>("VideoUrl")
-                        .HasColumnType("text")
-                        .HasColumnName("video_url");
 
                     b.HasKey("Id");
 
