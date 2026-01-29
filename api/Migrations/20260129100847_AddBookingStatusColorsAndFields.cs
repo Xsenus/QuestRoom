66ffee1ea62a4f4e9efeba9b64fe0c93a95cd0cf
Ilya <ilel@list.ru>
2026-01-29 17:11:24 +0700

    migrations add AddBookingStatusColorsAndFields

---
+++ api/Migrations/20260129100847_AddBookingStatusColorsAndFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingStatusColorsAndFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
