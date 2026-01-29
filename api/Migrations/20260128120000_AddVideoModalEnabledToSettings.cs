4243f9de5d5b94ecdc6e8c1098f3cf5d65151bdf
Ilya <ilel@list.ru>
2026-01-29 09:21:39 +0700

    Persist video modal setting in backend

---
+++ api/Migrations/20260128120000_AddVideoModalEnabledToSettings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVideoModalEnabledToSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "video_modal_enabled",
+                table: "settings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "video_modal_enabled",
+                table: "settings");
+        }
+    }
+}
