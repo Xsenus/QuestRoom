ee9761f006dcdb3ffad5b1fe14e3de787cabee1f
Ilya <ilel@list.ru>
2026-01-30 13:59:30 +0700

    migrations add AddSettingsScheduleBackground

---
+++ api/Migrations/20260130065741_AddSettingsScheduleBackground.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSettingsScheduleBackground : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "schedule_background",
+                table: "settings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "schedule_background",
+                table: "settings");
+        }
+    }
+}
