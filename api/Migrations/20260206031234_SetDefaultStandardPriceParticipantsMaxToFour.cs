a9c5edde1de2572272a555197e2e8fd6b1902222
Ilya <ilel@list.ru>
2026-02-06 10:38:03 +0700

    add SetDefaultStandardPriceParticipantsMaxToFour

---
+++ api/Migrations/20260206031234_SetDefaultStandardPriceParticipantsMaxToFour.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetDefaultStandardPriceParticipantsMaxToFour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "standard_price_participants_max",
+                table: "quests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "standard_price_participants_max",
+                table: "quests");
+        }
+    }
+}
