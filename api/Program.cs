5148a8a05718c91cc37d10a51dcc19c3610f5747
Ilya <ilel@list.ru>
2026-02-10 00:17:12 +0700

    fix

--- api/Program.cs
+++ api/Program.cs
@@ -112,7 +112,9 @@ builder.Services.AddCors(options =>
                     "http://localhost:5173",
                     "https://localhost:5173",
                     "http://localhost:3000",
-                    "https://localhost:3000")
+                    "https://localhost:3000",
+                    "http://127.0.0.1:5173",
+                    "https://127.0.0.1:5173")
                 .AllowAnyMethod()
                 .AllowAnyHeader();
 
