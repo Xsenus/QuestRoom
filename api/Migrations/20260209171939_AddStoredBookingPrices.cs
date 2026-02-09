cff8b3d630de114aaafba4d3161fa0ac69fc481e
Ilya <ilel@list.ru>
2026-02-10 00:22:05 +0700

    migrations add AddStoredBookingPrices

---
+++ api/Migrations/20260209171939_AddStoredBookingPrices.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestRoomApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoredBookingPrices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "extra_participant_price",
+                table: "bookings",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "quest_price",
+                table: "bookings",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "extra_participant_price",
+                table: "bookings");
+
+            migrationBuilder.DropColumn(
+                name: "quest_price",
+                table: "bookings");
+        }
+    }
+}
