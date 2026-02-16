b6e1ec9d39cddbb8e9205398de4378fa708b1fc6
Ilya <ilel@list.ru>
2026-02-17 02:11:05 +0700

    Remove migration file for holiday template mode change

--- api/DTOs/Schedule/QuestScheduleSettingsUpsertDto.cs
+++ api/DTOs/Schedule/QuestScheduleSettingsUpsertDto.cs
@@ -4,4 +4,6 @@ public class QuestScheduleSettingsUpsertDto
 {
     public Guid QuestId { get; set; }
     public int? HolidayPrice { get; set; }
+    public string HolidayPricingMode { get; set; } = "fixed_price";
+    public int? HolidayTemplateDayOfWeek { get; set; }
 }
